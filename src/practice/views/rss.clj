ab63882efc4f24bb4f5e963a78261aedc0e89153
Craig Brozefsky <craig@red-bean.com>
2011-12-16 02:30:00 -0600

    Spruced up the feeds, including properly formatted dates.

--- src/practice/views/rss.clj
+++ src/practice/views/rss.clj
@@ -1,3 +1,13 @@
+;; RSS Feed Support
+;; We are working from the RSS spec at:
+;; http://cyber.law.harvard.edu/rss/index.html
+;;
+;; This is a simple RSS feed generator that puts the blog posts in
+;; reverse chrono order.  It's a full text feed, with the whole post
+;; in the description field.  I like prefer this as a user, as it
+;; means I can DL the feed and read offline, like on my phone with
+;; spotty reception.
+
 (ns practice.views.rss
   (:use noir.core
         noir.request
@@ -6,19 +16,36 @@
         hiccup.core)
   (:require [clojure.xml :as xml]))
 
-(defn make-channel [title link description & optional]
+(def max-posts
+  "Max posts to put in the rss feed."
+  42)
+
+(defn date-as-rfc822 [#^java.util.Date date]
+  (.format
+   (java.text.SimpleDateFormat.
+    "EEE', 'dd' 'MMM' 'yyyy' 'HH:mm:ss' 'Z",)
+   date))
+
+(defn blog-channel [items]
   {:tag "channel"
-   :attrs {}
-   :content [{:tag "title" :content [title]}
-             {:tag "link" :content [link]}
-             {:tag "description" :content [description]}]})
+   :content (concat
+             [{:tag "title" :content ["Practice Blog"]}
+              {:tag "link" :content [(server-url)]}
+              {:tag "language" :content ["en-us"]}
+              {:tag "webMaster" :content ["craig@red-bean.com (Craig Brozefsky)"]}
+              {:tag "managingEditor" :content ["craig@red-bean.com (Craig Brozefsky)"]}
+              {:tag "copyright" :content ["Public Domain"]}
+              {:tag "description" :content ["A place to prace sharing."]}]
+             items)})
 
 (defn post-rss-item [p]
+  "Given a post, return a XML item tag (as a map) representing the post.  We put the full text in the description"
   {:tag "item"
    :content [{:tag "title" :content [(post-title p)]}
              {:tag "link" :content [(str (server-url) "/posts/" (:name p))]}
              {:tag "guid" :content [(str (server-url) "/posts/" (:name p))]}
-             {:tag "pubDate" :content [(:date (:metadata p))]}
+             {:tag "pubDate" :content [(date-as-rfc822
+                                        (java.util.Date. (:date (:metadata p))))]}
              {:tag "author" :content ["craig@red-bean.com (Craig Brozefsky)"]}
              {:tag "description" :content [(escape-html (post-body p))]}]})
 
@@ -28,7 +55,7 @@
     (concat (:content tag)
             content)))
 
-(defn make-rss [channel]
+(defn rss-feed [channel]
   {:tag "rss"
    :attrs {"version" "2.0"}
    :content [channel]})
@@ -36,18 +63,12 @@
 (defn feed-body []
   (with-out-str
     (clojure.xml/emit
-     (make-rss
-      (add-content
-       (make-channel "Practice Blog"
-                     (server-url)
-                     "A place to share")
+     (rss-feed
+      (blog-channel
        (map
         post-rss-item
-        posts))))))
+        (take max-posts posts)))))))
 
 (defpage [:get "/rss"] []
   {:headers {"Content-Type" "application/rss+xml"}
    :body (feed-body)})
-
-
-        
